1a9fdbc75f006dacd23c5c87a1b756f9f0ce2007
Felipe Bergamin <felipebergamin6@gmail.com>
2019-10-25 14:47:53 -0300

    mock useDispatch

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { render } from "@testing-library/react";
+import { useSelector, useDispatch } from "react-redux";
+import { render, fireEvent } from "@testing-library/react";
 
 import TechList from "~/components/TechList";
 
@@ -14,9 +14,26 @@ describe("TechList Component", () => {
       })
     );
 
-    const { getByText, getByTestId, debug } = render(<TechList />);
+    const { getByText, getByTestId /*, debug */ } = render(<TechList />);
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
     expect(getByTestId("tech-list")).toContainElement(getByText("ReactJS"));
-    debug();
+    // debug();
+  });
+
+  it("should be able to add new techs", () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
+
+    // console.log(dispatch.mock.calls);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "ADD_TECH",
+      payload: { tech: "Node.js" }
+    });
   });
 });
